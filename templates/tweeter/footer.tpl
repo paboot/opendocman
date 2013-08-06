9fb9e2d81e3619dab3a594fd4758ec73b9e5f724
paboot <13510040@std.stei.itb.ac.id>
2013-08-06 13:59:12 +0700

    first commit

---
+++ templates/tweeter/footer.tpl
@@ -0,0 +1,30 @@
+{if $g_demo eq 'True'}
+<script type="text/javascript"><!--
+ google_ad_client = "ca-pub-3696425351841264";
+ /* 728x90_ODM_Demo */
+ google_ad_slot = "8419809005";
+ google_ad_width = 728;
+ google_ad_height = 90;
+ //-->
+ </script>
+ <script type="text/javascript"
+ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+ </script>
+{/if}
+	</div> <!-- /container -->   
+      <footer>
+          <!--<a href="http://www.opendocman.com/">
+                <img src="{$g_base_url}/images/logo.gif" title="{$site_title}" alt="{$site_title}" border="0">
+            </a>-->
+        <span id="footer-text">Copyright &copy; 2000-2013 Stephen Lawrence. Design by Pabot & Fandi</span>
+        <ul id="footer-list">
+			<li><a href="http://www.opendocman.com/" target="_new">OpenDocMan v{$g_current_version}</a></li>
+            <li><a href="http://www.opendocman.com/forum/" target="_new">Support</a></li>
+            <li><a href="http://opendocman.uservoice.com" target="_new">Feedback</a></li>
+            <li><a href="https://github.com/opendocman/opendocman/issues" target="_new">Bugs</a></li>
+        </ul>
+      </footer>
+    <script type="text/javascript" src="{$g_base_url}/templates/tweeter/js/bootstrap.min.js"></script>
+</body>
+</html>
+
---
+++ templates_c/%%0E^0E4^0E407559%%footer.tpl.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version 2.6.26, created on 2013-08-06 08:40:52
+         compiled from footer.tpl */ ?>
+<?php if ($this->_tpl_vars['g_demo'] == 'True'): ?>
+<script type="text/javascript"><!--
+ google_ad_client = "ca-pub-3696425351841264";
+ /* 728x90_ODM_Demo */
+ google_ad_slot = "8419809005";
+ google_ad_width = 728;
+ google_ad_height = 90;
+ //-->
+ </script>
+ <script type="text/javascript"
+ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+ </script>
+<?php endif; ?>
+	</div> <!-- /container -->   
+      <footer>
+          <!--<a href="http://www.opendocman.com/">
+                <img src="<?php echo $this->_tpl_vars['g_base_url']; ?>
+/images/logo.gif" title="<?php echo $this->_tpl_vars['site_title']; ?>
+" alt="<?php echo $this->_tpl_vars['site_title']; ?>
+" border="0">
+            </a>-->
+        <span id="footer-text">Copyright &copy; 2000-2013 Stephen Lawrence. Design by Pabot & Fandi</span>
+        <ul id="footer-list">
+			<li><a href="http://www.opendocman.com/" target="_new">OpenDocMan v<?php echo $this->_tpl_vars['g_current_version']; ?>
+</a></li>
+            <li><a href="http://www.opendocman.com/forum/" target="_new">Support</a></li>
+            <li><a href="http://opendocman.uservoice.com" target="_new">Feedback</a></li>
+            <li><a href="https://github.com/opendocman/opendocman/issues" target="_new">Bugs</a></li>
+        </ul>
+      </footer>
+    <script type="text/javascript" src="<?php echo $this->_tpl_vars['g_base_url']; ?>
+/templates/tweeter/js/bootstrap.min.js"></script>
+</body>
+</html>
